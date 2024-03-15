37f645f52e78038ad07ff34441589f08227d36c0
bagasdhitya98 <bagasdhityataufiqqi21@gmail.com>
2024-03-15 16:50:48 +0700

    add: callback for authorizing oauth

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+// App.js
+import React, { useState, useEffect } from "react";
 import Tesseract from "tesseract.js";
-// import { google } from "googleapis";
+import authorize from "./services/authorize";
+import writeToSpreadsheet from "./services/writeToSpreadsheet";
 
 const App = () => {
   const [image, setImage] = useState(null);
@@ -14,56 +16,6 @@ const App = () => {
   const cleanText = (rawText) => {
     return rawText.replace(/[^a-zA-Z0-9\s\/]/g, "");
   };
-
-  // const authorize = async () => {
-  //   const client = new google.auth.OAuth2(
-  //     "YOUR_CLIENT_ID",
-  //     "YOUR_CLIENT_SECRET",
-  //     "YOUR_REDIRECT_URL"
-  //   );
-
-  //   const authUrl = client.generateAuthUrl({
-  //     access_type: "offline",
-  //     scope: ["https://www.googleapis.com/auth/spreadsheets"],
-  //   });
-
-  //   // Redirect ke authUrl dan dapatkan authorization code
-  //   // Lalu gunakan authorization code untuk mendapatkan token
-  //   // Contoh: const token = await client.getToken(authorizationCode);
-  //   // Setelah itu, gunakan token untuk membuat client yang siap digunakan
-  //   // Contoh: client.setCredentials(token.tokens);
-  // };
-
-  // const writeToSpreadsheet = async (nik, nama, ttl) => {
-  //   const client = new google.auth.OAuth2(
-  //     "YOUR_CLIENT_ID",
-  //     "YOUR_CLIENT_SECRET",
-  //     "YOUR_REDIRECT_URL"
-  //   );
-  //   const sheets = google.sheets({ version: "v4", auth: client });
-  //   const spreadsheetId = "YOUR_SPREADSHEET_ID";
-  //   const range =
-  //     "AEON Alam Sutera In Klinik Galaxy Tangerang!NIK:NAMA:TANGGAL LAHIR";
-
-  //   const values = [[nik, nama, ttl]];
-
-  //   const resource = {
-  //     values,
-  //   };
-
-  //   try {
-  //     const response = await sheets.spreadsheets.values.append({
-  //       spreadsheetId,
-  //       range,
-  //       valueInputOption: "RAW",
-  //       resource,
-  //     });
-
-  //     console.log(`${response.data.updates.updatedCells} cells updated.`);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
 
   const detectText = () => {
     if (!image) {
@@ -101,13 +53,17 @@ const App = () => {
         });
 
         setText(`NIK: ${nik}\nNama: ${nama}\nTempat/Tgl Lahir: ${ttl}`);
-        // writeToSpreadsheet(nik, nama, ttl);
+        writeToSpreadsheet(nik, nama, ttl);
       })
       .catch((error) => {
         alert("Deteksi teks gagal. Silakan coba lagi.");
         console.error(error);
       });
   };
+
+  useEffect(() => {
+    authorize();
+  }, []);
 
   return (
     <div>
