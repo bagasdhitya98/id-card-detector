b8d9e9bf6505ad9b3ef467f041a296b397a0083a
bagasdhitya98 <bagasdhityataufiqqi21@gmail.com>
2024-03-18 17:50:32 +0700

    refactor: writespreadsheet callback

--- src/services/writeToSpreadsheet.js
+++ src/services/writeToSpreadsheet.js
@@ -5,33 +5,41 @@ const writeToSpreadsheet = async (
   accessToken,
   spreadsheetId
 ) => {
-  const token = accessToken;
-  const id = spreadsheetId;
-  const range = "Sheet1!A1:C1";
+  try {
+    const token = accessToken;
+    const id = spreadsheetId;
+    const range = "Sheet1!A1:C1";
 
-  const values = [[nik, nama, ttl]];
+    const values = [[nik, nama, ttl]];
 
-  const response = await fetch(
-    `https://sheets.googleapis.com/v4/spreadsheets/${id}/values/${range}:append?valueInputOption=RAW`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        range,
-        majorDimension: "ROWS",
-        values,
-      }),
+    const response = await fetch(
+      `https://sheets.googleapis.com/v4/spreadsheets/${id}/values/${range}:append?valueInputOption=RAW`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          range,
+          majorDimension: "ROWS",
+          values,
+        }),
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      return {
+        message: `${data.updates.updatedCells} cells updated.`,
+        status: "success",
+      };
     }
-  );
-
-  if (response.ok) {
-    const data = await response.json();
-    console.log(`${data.updates.updatedCells} cells updated.`);
-  } else {
-    console.error("Failed to update cells:", response.statusText);
+  } catch (error) {
+    return {
+      message: "An error occurred : " + error,
+      status: "error",
+    };
   }
 };
 
